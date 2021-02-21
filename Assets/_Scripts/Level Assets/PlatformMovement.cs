f1026995a11ef55cbfe9cf5fe3f15b0fbe20d88b
Bojarzin <47534935+Bojarzin@users.noreply.github.com>
2021-02-21 00:31:01 -0500

    More additions to level construction, as well as a new falling platform type

--- Assets/_Scripts/Level Assets/PlatformMovement.cs
+++ Assets/_Scripts/Level Assets/PlatformMovement.cs
@@ -12,6 +12,7 @@ public class PlatformMovement : MonoBehaviour
         MOVE,       // Simple movement
         ELEVATOR,   // Simple movement + brief pause at each point (currently unused)
         FLIP,       // Flips over after a pause
+        FALLING     // Pauses, then falls after being landed on
     };
 
     [SerializeField] PlatformType platformType;
@@ -32,10 +33,16 @@ public class PlatformMovement : MonoBehaviour
 
     [SerializeField] float rotateSpeed = 1.0f;
     [SerializeField] float flipStart = 0.0f;
-    bool hold = true;
+
+    bool hold = true;                   // First four currently unused, intended to control the pause for certain platforms
     float flipTimer = 2.5f;
     float holdPositionTimer = 5.0f;
     bool move = false;
+
+    ///  Used for FALLING
+    bool fall = false;          // Set to true when ready to fall
+    bool shake = false;         // Shake "animation" to indicate fall incoming
+    float fallSpeedIncrease;    // Makes the platform increase as it falls rather than moving down like an elevator
 
 
     // Start is called before the first frame update
@@ -43,7 +50,7 @@ public class PlatformMovement : MonoBehaviour
     {
         player = GameObject.FindObjectOfType<PlayerMovement>();
 
-        if (platformType == PlatformType.MOVE || platformType == PlatformType.ELEVATOR)
+        if (platformType == PlatformType.MOVE || platformType == PlatformType.ELEVATOR || platformType == PlatformType.FALLING)
         {
             positionOne = transform.position;
             positionTwoV = positionTwoT.position;
@@ -55,6 +62,8 @@ public class PlatformMovement : MonoBehaviour
     {
         Move();
         Rotate();
+        Shake();
+        Fall();
     }
 
     void Move()
@@ -74,13 +83,64 @@ public class PlatformMovement : MonoBehaviour
         }
     }
 
+    void Fall()
+    {
+        if (platformType == PlatformType.FALLING)
+        {
+            if (fall)
+            {
+                fallSpeedIncrease += 0.05f;
+                transform.position = transform.position - new Vector3(0.0f, moveSpeed * Time.deltaTime * fallSpeedIncrease, 0.0f);
+            }
+        }
+    }
+
+    void Shake()
+    {
+        if (platformType == PlatformType.FALLING)
+        {
+            if (shake)
+            {
+                Vector3 shakeLeft = positionOne - new Vector3(0.08f, 0.0f, 0.0f);
+                Vector3 shakeRight = positionOne + new Vector3(0.08f, 0.0f, 0.0f);
+                transform.position = Vector3.Lerp(shakeLeft, shakeRight, Mathf.PingPong(Time.time * moveSpeed * 10, 1.0f));
+            }
+        }
+    }
+
+    IEnumerator ActivateFall()
+    {
+        if (platformType == PlatformType.FALLING)
+        {
+            yield return new WaitForSeconds(0.5f);
+            shake = false;
+
+            yield return new WaitForSeconds(0.5f);
+
+            fall = true;
+
+            yield return new WaitForSeconds(2.5f);
+
+            fall = false;
+
+            fallSpeedIncrease = 0.0f;
+            transform.position = positionOne;
+        }
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            if (platformType != PlatformType.BASIC)
+            if (platformType != PlatformType.BASIC && platformType != PlatformType.FALLING)
             {
-                collision.gameObject.transform.SetParent(gameObject.transform);
+               collision.gameObject.transform.SetParent(gameObject.transform);
+            }
+
+            if (platformType == PlatformType.FALLING && !fall)
+            {
+                shake = true;
+                StartCoroutine(ActivateFall());
             }
         }
     }
